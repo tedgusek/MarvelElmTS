40da642f502190b81fde1555762f31fa778b67ad
Ted Gusek <theohgusek@gmail.com>
2023-05-18 15:33:09 -0400

    still working on connecting elm to backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,15 @@
 import marvelController from './controllers/marvelController';
 import { CustomResponse } from './types';
 import express, { Express, Request, Response, NextFunction } from 'express';
+// import path from 'path';
+
+// const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 const app: Express = express();
 const PORT: number = 5173;
+
+// app.use(express.static(path.resolve(__dirname, '../../frontend/src')));
+// app.use(express.static(path.resolve(__dirname, '../../frontend/src')));
 
 // Middleware
 app.use(express.json() as NextFunction);
@@ -16,9 +22,12 @@ app.get('/characters', marvelController.getCharacters);
 app.use((err: any, req: Request, res: Response) => {
   console.log(err.stack);
   (res as CustomResponse).status(500).json({ error: 'Internal Server Error' });
+  return;
 });
 
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
---
+++ frontend/src/index.js
@@ -0,0 +1,5 @@
+import { Elm } from './src/elm.js';
+import 'vite/modulepreload-polyfill';
+
+// const app = Elm.Main.init({});
+const app = document.getElementById('elm-container');
